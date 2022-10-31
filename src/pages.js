31556834caf951e7e35cd10255bf7ab01b7fb89e
jv-aquino <jvaquinocontact@gmail.com>
2022-10-31 17:17:25 -0300

    + basic contato div

--- src/pages.js
+++ src/pages.js
@@ -24,7 +24,18 @@ const contato = () => {
   const h1 = document.createElement("h1");
   h1.textContent = "Contato";
 
+  const contactDiv = document.createElement("div");
+  contactDiv.classList.add("grid", "contact");
+  contactDiv.innerHTML = "<p class='m-auto'>STMA</p>";
+  contactDiv.innerHTML += "<p><span>(12) 3123-2733</span></p>";
+  contactDiv.innerHTML += "<p><a href='mailto:sma.feg@unesp.br'>sma.feg@unesp.br</a></p>";
+
+  const funcionariosDiv = document.createElement("div");
+  funcionariosDiv.classList.add("grid", "funcionarios");
+
   main.appendChild(h1);
+  main.appendChild(contactDiv);
+  main.appendChild(funcionariosDiv);
 
   return main;
 }
