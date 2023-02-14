66c1ecfa8285562a7fd0f0d426d4890c0f41dc74
jv-aquino <jvaquinocontact@gmail.com>
2023-02-13 22:13:53 -0300

    + sobre section template

--- src/components/Main/Sobre.js
+++ src/components/Main/Sobre.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import fotoSTMA from '../../img/stma.png';
 
 export default function Sobre() {
   return (
     <>
       <h1>STMA</h1>
       
+      <div className='flex sobre gap-6'>
+        <img src={fotoSTMA} alt="Imagem de exemplo" />
+        <div className='drop-shadow-xl'>
+          <p>A Seção Técnica de Materiais da FEG é responsável pela <b>compra</b> de materiais e serviços necessários para o funcionamento da Faculdade.</p>
+        </div>
+      </div>
     </>
   )
 }
