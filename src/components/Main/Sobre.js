54c58ab49161db91d616e28bf969f044b100c034
jv-aquino <jvaquinocontact@gmail.com>
2023-02-15 15:13:46 -0300

    + update sobre template & fix some bugs

--- src/components/Main/Sobre.js
+++ src/components/Main/Sobre.js
@@ -7,10 +7,22 @@ export default function Sobre() {
     <>
       <h1>STMA</h1>
       
-      <div className='flex sobre gap-6'>
-        <img src={fotoSTMA} alt="Imagem de exemplo" />
-        <div className='drop-shadow-xl'>
-          <p>A Seção Técnica de Materiais da FEG é responsável pela <b>compra</b> de materiais e serviços necessários para o funcionamento da Faculdade.</p>
+      <div className='sobre'>
+        <div className="grid">
+          <img src={fotoSTMA} alt="Imagem de exemplo" />
+
+          <div className='drop-shadow-xl text'>
+            <p>A Seção Técnica de Materiais da FEG é responsável pela <b>compra</b> de materiais e serviços necessários para o funcionamento da Faculdade.</p>
+          </div>
+        </div>
+
+        
+        <div className="grid">
+          <div className='drop-shadow-xl text'>
+            <p>A Seção Técnica de Materiais da FEG é responsável pela <b>compra</b> de materiais e serviços necessários para o funcionamento da Faculdade.</p>
+          </div>
+
+          <img src={fotoSTMA} alt="Imagem de exemplo" />
         </div>
       </div>
     </>
