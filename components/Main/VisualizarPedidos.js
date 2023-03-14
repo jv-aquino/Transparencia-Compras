c1107087c2cd2e37663e5a629c7d924e9b1413a1
jv-aquino <jvaquinocontact@gmail.com>
2023-03-14 10:25:00 -0300

    + basic visual pedido search

--- components/Main/VisualizarPedidos.js
+++ components/Main/VisualizarPedidos.js
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { supabase } from 'lib/supabaseClient';
 
 const searchPedido = async (pedido) => {
   let { data: pedidosEmCompras, error } = await supabase
   .from('pedidosEmCompras')
   .select('*')
-  .eq('id_pedido', pedido);
+  .eq('id_agrupamento', pedido);
 
   return pedidosEmCompras;
 }
 
+function Pedido({obj}) {
+  if (obj == undefined) {
+    return (
+    <div className="pedidoEncontrado">
+          <p>Pedido não encontrado :(</p>
+          <p>Pesquise o número da sua solicitação no SISADM:</p>
+          <ul className='list-disc'>
+            <li>Se estiver em compras mas não tiver número de agrupamento, seu pedido está na lista de <b>espera</b>, aguardando definição da modalidade de compra</li>
+            <li>Se o pedido tiver número de agrupamento, pesquise novamente com o número do agrupamento</li>
+          </ul>
+    </div>
+    )
+  }
+  return (
+    <div className='pedidoEncontrado'>
+      <p>Pedido n°{obj.id_agrupamento}</p>
+      <p className='status'>Status: {obj.status}</p>
+    </div>
+  )
+} 
+
 export default function VisualizarPedidos() {
+  const [visible, setVisible] = useState(false);
+  const [pedidoEncontrado, setPedido] = useState({});
+
   return (
     <>
       <h1>Pedidos</h1>
+
+      {visible && <Pedido obj={pedidoEncontrado} />}
 
       <button className="pedidos drop-shadow-lg" onClick={(e) => {
         e.target.classList.add('hidden');
@@ -21,20 +47,22 @@ export default function VisualizarPedidos() {
         document.querySelector('form.pesquisa').classList.remove('hidden');
         document.querySelector('form.pesquisa').classList.add('flex');
       }}>
-        Visualizar Pedidos
+        Visualizar Pedido
       </button>
 
-      <form className="pesquisa hidden flex-col p-4 gap-3 rounded-md">
-        <label htmlFor="id" className='a text-lg'>Número do <b>Pedido</b> ou <b>Agrupamento</b>:</label>
+      <form className="pesquisa hidden flex-col p-4 gap-3 rounded-md" onSubmit={(e) => {
+          e.preventDefault();
+          let pedido = document.querySelector("#id").value;
+          searchPedido(pedido)
+          .then(res => {
+            setPedido(res[0]);
+            setVisible(true);
+          })
+        }}>
+        <label htmlFor="id" className='text-[1.2rem]'>Número do <b>Agrupamento</b>:</label>
         <input type="text" name="id" id="id" maxLength={11} className='rounded p-2 border-2 border-black' />
 
-        <button className='search' type='button'
-        onClick={() => {
-          let pedido = document.querySelector("#id").value;
-          console.log(pedido)
-          searchPedido(pedido)
-          .then(res => console.log(res[0]))
-        }}>
+        <button className='search'>
           Pesquisar
         </button>
 
